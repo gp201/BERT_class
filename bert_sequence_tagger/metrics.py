779224798a18f61498b50930da904a5726e45ee5
PRANEETH <praneethg2001@gmail.com>
2020-04-06 23:18:36 +0530

    metrics edit

--- bert_sequence_tagger/metrics.py
+++ bert_sequence_tagger/metrics.py
@@ -1,6 +1,7 @@
 import itertools
 from sklearn.metrics import f1_score as f1_score_sklearn
 from seqeval.metrics import f1_score
+from sklearn.metrics import classification_report
 
 
 def f1_entity_level(*args, **kwargs):
@@ -16,4 +17,14 @@ def f1_token_level(true_labels, predictions):
     return f1_score_sklearn(true_labels, 
                             predictions, 
                             average='micro',
+                            labels=labels)
+
+def f1_per_token(true_labels, predictions):
+    true_labels = list(itertools.chain(*true_labels))
+    predictions = list(itertools.chain(*predictions))
+    
+    labels = list(set(true_labels) - {'[PAD]', 'O'})
+    
+    return classification_report(predictions,
+                            true_labels, 
                             labels=labels)
