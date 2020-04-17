e13e430c793a73f0193a2aba17e98e30e7602fc3
PRANEETH <praneethg2001@gmail.com>
2020-04-17 20:36:43 +0530

    Add files via upload

--- bert_sequence_tagger/metrics.py
+++ bert_sequence_tagger/metrics.py
@@ -1,7 +1,9 @@
 import itertools
+import numpy as np
 from sklearn.metrics import f1_score as f1_score_sklearn
 from seqeval.metrics import f1_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 def f1_entity_level(*args, **kwargs):
@@ -34,9 +36,14 @@ def f1_per_token_plot(true_labels, predictions):
     predictions = list(itertools.chain(*predictions))
     
     labels = list(set(true_labels) - {'[PAD]', 'O'})
+    c_labels = list(set(true_labels) - {'[PAD]'})
+    #confusion_matrix for accuracy
+    cm = confusion_matrix(true_labels, predictions, labels = c_labels)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    accuracy = cm.diagonal()
     
     return classification_report(predictions,
                             true_labels, 
                             labels=labels,
-                            output_dict=True)
+                            output_dict=True),accuracy,c_labels
                             
